e9903db40e61790d33316fd69438eedd63c1591f
narduc <radnielpadron@gmail.com>
2020-07-16 10:30:55 -0700

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+    arr = []
+    return arr
+end
+
+def array_with_two_elements
+    arr = [1,2]
+    return arr
+end
+
+# indexing
+def first_element(arr)
+    return arr.first
+end
+
+#   takes in an argument of an array and returns the first element in the array using its positive index (FAILED - 1)
+def third_element(arr)
+    return arr[2]
+end
+#   takes in an argument of an array and returns the third element in the array using its positive index (FAILED - 2)
+def last_element(arr)
+    return arr.last
+end
+#   takes in an argument of an array and returns the last element in the array using its negative index (FAILED - 3)
+# using ruby array methods to return values from an array
+def first_element_with_array_methods(arr)
+    return arr.first
+end
+#   takes in an argument of an array and returns the first element in the array, without referencing the index number of that element (FAILED - 4)
+def last_element_with_array_methods(arr)
+    return arr.last
+end
+#   takes in an argument of an array and returns the last element in the array, wihtout referencing the index number (FAILED - 5)
+# using ruby array methods to get information about an array
+def length_of_array(arr)
+    return arr.size
+end
+#   takes in an argrument of an array and returns the length of the array (FAILED - 6)
+
